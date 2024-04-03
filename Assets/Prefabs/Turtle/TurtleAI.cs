e6ad3b3b12191cb17687d2b923d2aba5eba39183
Liam <rockl@wwu.edu>
2024-04-03 12:38:01 -0700

    Turtles eat
    Turtles will now seek out and eat Cherry's within their range

--- Assets/Prefabs/Turtle/TurtleAI.cs
+++ Assets/Prefabs/Turtle/TurtleAI.cs
@@ -7,6 +7,7 @@ public class TurtleAI : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     [SerializeField] private Transform targetDestination;
+    private SphereCollider range; // Vision range, affected by awareness genetics
 
     // Genetics
     [SerializeField] private float socialability = 1.0f;
@@ -16,20 +17,44 @@ public class TurtleAI : MonoBehaviour
     [SerializeField] private float attraction = 1.0f;
     [SerializeField] private float resistance = 1.0f;
     [SerializeField] private float immunity = 1.0f;
-    [SerializeField] private float awareness = 1.0f;
+    public float awareness = 7.0f;
 
     // Stats
     [SerializeField] private int fullness = 100;
+    [SerializeField] private GameObject food;
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        range = GetComponent<SphereCollider>();
         navMeshAgent.speed = speed;
+        navMeshAgent.destination = targetDestination.position;
 
         StartCoroutine("DepleteFullness");
     }
 
-    private void Update() {
-        navMeshAgent.destination = targetDestination.position;
+    public void SetDestination(Vector3 destination) 
+    {
+        navMeshAgent.destination = destination;
+    }
+
+    public void SetFood(GameObject food) 
+    {
+        this.food = food;
+        navMeshAgent.destination = this.food.transform.position;
+    }
+
+    private void Update() 
+    {
+        if(food)
+        {
+            if (Vector3.Distance(transform.position, food.transform.position) <  0.5f)
+            {
+                Eat(10);
+                Destroy(food);
+                food = null;
+            }
+        }
+        
     }
 
     // every 2 seconds perform the print()
@@ -44,7 +69,9 @@ public class TurtleAI : MonoBehaviour
 
     private void Eat(float satiation)
     {
-        fullness = Mathf.Clamp(fullness + satiation, 0, 100);
-    }
+        Debug.Log("Eating for " + satiation + " satiation");
+        fullness = (int) Mathf.Clamp(fullness + satiation, 0, 100);
 
+        navMeshAgent.destination = targetDestination.position;
+    }
 }
